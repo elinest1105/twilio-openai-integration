216373480b0de105d961c5bb9f86ba0415f0e24c
happywebstar <james.cobb812@gmail.com>
2023-05-25 20:00:57 +0000

    feat: update structure

--- functions/init.js
+++ functions/init.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 const openai = require("openai");
-const serviceAccount = require("..twilio-openai-d6a44-firebase-adminsdk-vde5o-1cefa4723e.json");
+const serviceAccount = require("../Assets/twilio-openai-d6a44-firebase-adminsdk-vde5o-1cefa4723e.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
